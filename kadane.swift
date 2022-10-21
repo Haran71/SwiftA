2ee19d86a0e7fe7e02f23b8696f52ea97c4a98ed
Haran Raajesh <haran@Harans-MacBook-Pro.local>
2022-10-22 03:50:18 +0530

    Added initial version of Kadane's algorithmn

---
+++ kadane.swift
@@ -0,0 +1,25 @@
+func max(A:Int,B:Int) -> Int {
+    return A > B ? A:B;
+}
+
+let n:Int? = Int(readLine()!);
+
+var arr = [Int]();
+
+for _ in 1...n!{
+    let a:Int? = Int(readLine()!);
+    arr.append(a!);
+}
+
+var result:Int = -99999;
+var Current:Int = 0;
+
+for i in 0...n!-1{
+    Current += arr[i];
+    result = max(A: result, B: Current);
+    if (Current < 0){
+        Current = 0;
+    }
+}
+print(result);
+
